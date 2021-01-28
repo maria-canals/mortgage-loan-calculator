5fdd6aebc9437c330181a943185596bbed3f7809
bcn-alumno <bcn-alumno@nm_madrid.local>
2021-01-28 14:31:24 +0100

    first commit

--- loancalculator.js
+++ loancalculator.js
@@ -1,12 +1,25 @@
-function computeLoan(){
-    const amount = document.querySelector('#amount').value;
-    const interest_rate = document.querySelector('#interest_rate').value;
-    const months = document.querySelector('#months').value;
-    const interest = (amount * (interest_rate * 0.01)) / months;
-    let payment = ((amount / months) + interest).toFixed(2); //calculate monthly payment
-
-    //regedit to add a comma after every three digits
-    payment = payment.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ","); //\B looks for a word boundary, ? says what to look for, \d looks for 3 digits in a row
-    document.querySelector('#payment').innerHTML = `Monthly Payment = ${payment}` 
+const App = {
+    data() {
+      return {
+        payment: 0,
+        isMonthlyPaimentVisible: false,
+        loanAmout: "",
+        interestRate: "",
+        monthsToPay: "",
+        result: "",
+        finalResult: ""
+      }
+    },
     
-}
+    methods:{
+        getInputs(){
+            let interest = this.loanAmout * (this.interestRate / 100)
+            let loanPlusInterest = Number(this.loanAmout) + interest
+            this.result = loanPlusInterest / Number(this.monthsToPay).toFixed(2)
+            this.finalResult = this.result.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")
+        }
+       
+    }
+  }
+  
+  Vue.createApp(App).mount('#loancal')
