c832c3d3056af75c3aa29bb10e21db02d1379817
maria-canals <mariacanals23@gmail.com>
2021-02-03 23:42:20 +0100

    solved

--- loancalculator.js
+++ loancalculator.js
@@ -1,22 +1,27 @@
 const App = {
     data() {
       return {
-        payment: 0,
-        isMonthlyPaimentVisible: false,
-        loanAmout: "",
+        loanAmount: "",
         interestRate: "",
         monthsToPay: "",
         result: "",
-        finalResult: ""
       }
     },
     
     methods:{
         getInputs(){
-            let interest = this.loanAmout * (this.interestRate / 100)
-            let loanPlusInterest = Number(this.loanAmout) + interest
-            this.result = loanPlusInterest / Number(this.monthsToPay).toFixed(2)
-            this.finalResult = this.result.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")
+        const loanAmount = this.loanAmount
+        const interest = loanAmount * (this.interestRate / 100)
+        const months = this.monthsToPay
+
+        if(interest && loanAmount && months){
+          this.calculateMortgage(interest, months, loanAmount)
+        }},
+
+        calculateMortgage(interest, months, loanAmount){
+          let payment = ((Number(loanAmount) + interest) / months).toFixed(2)
+          payment = payment.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")
+          this.result = payment
         }
        
     }
